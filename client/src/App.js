3a3cbf07b373c198d8930462ba39af7296e31491
Srimanta Bagchi <srimantabagchi@gmail.com>
2019-12-02 11:46:25 -0500

    Load user and set auth token

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,40 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Register from "./pages/register/register";
 import SignIn from "./pages/signin/signin";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import ResetPassword from "./pages/resetpassword/resetpassword";
+import { loadUser } from "./actions/auth";
+import setAuthToken from "./utils/setAuthToken";
 
 // Redux
 import { Provider } from "react-redux";
 import store from "./store";
 import Navbar from "./layouts/navbar";
 
-const App = () => (
-  <Provider store={store}>
-    <Router>
-      <Fragment>
-        <Navbar />
-        <div className='App'>
-          <Route exact path='/' component={SignIn} />
-          <Route path='/register' component={Register} />
-          <Route path='/resetpassword' component={ResetPassword} />
-        </div>
-      </Fragment>
-    </Router>
-  </Provider>
-);
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+  return (
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <div className='App'>
+            <Route exact path='/' component={SignIn} />
+            <Route path='/register' component={Register} />
+            <Route path='/resetpassword' component={ResetPassword} />
+          </div>
+        </Fragment>
+      </Router>
+    </Provider>
+  );
+};
 
 export default App;
