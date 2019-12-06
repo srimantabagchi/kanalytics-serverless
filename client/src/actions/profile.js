74bd8e3bae408eb1f58da09fdc65a98451fac11b
Srimanta Bagchi <srimantabagchi@gmail.com>
2019-12-06 15:33:36 -0500

    Interim commit

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -19,3 +19,56 @@ export const getCurrentProfile = () => async dispatch => {
     });
   }
 };
+/*
+// Add Experience
+export const addFiles = (formData, history) => async dispatch => {
+  try {
+    const config = {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    };
+
+    const res = await axios.put("/api/profile/files", formData, config);
+
+    dispatch({
+      type: ADD_FILE,
+      payload: res.data
+    });
+
+    dispatch(setAlert("File Added", "success"));
+
+    history.push("/dashboard");
+  } catch (err) {
+    const errors = err.response.data.errors;
+
+    if (errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, "danger")));
+    }
+
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+// Delete experience
+export const deleteFile = id => async dispatch => {
+  try {
+    const res = await axios.delete(`/api/profile/file/${id}`);
+
+    dispatch({
+      type: DELETE_FILE,
+      payload: res.data
+    });
+
+    dispatch(setAlert("Experience Removed", "success"));
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+*/
